682293e0c6814768d4cca71e3bc8b66d7f210277
Israel Manzo <israelmanzo814@gmail.com>
2020-05-05 13:55:59 -0400

    Added tab bar controller

---
+++ DevPost/DevPost/Controller/TabBar/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  DevPost
+//
+//  Created by Israel Manzo on 5/5/20.
+//  Copyright © 2020 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
