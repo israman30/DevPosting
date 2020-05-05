48602929616364117bf2f482d9815142613ec7fd
Israel Manzo <israelmanzo814@gmail.com>
2020-05-05 14:31:03 -0400

    Added background color to profile controller

--- DevPost/DevPost/Controller/ProfileController.swift
+++ DevPost/DevPost/Controller/ProfileController.swift
@@ -12,6 +12,6 @@ class ProfileController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor.mainColor()
     }
 }
