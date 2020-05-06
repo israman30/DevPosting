e8eb21975c26e03a1b12337431a283ded8ac488c
Israel Manzo <israelmanzo814@gmail.com>
2020-05-05 22:29:48 -0400

    Updating profile ui

--- DevPost/DevPost/Controller/ProfileController.swift
+++ DevPost/DevPost/Controller/ProfileController.swift
@@ -10,8 +10,64 @@ import UIKit
 
 class ProfileController: UIViewController {
     
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = .systemFont(ofSize: 30)
+        label.isEnabled = true
+        return label
+    }()
+    
+    let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "johndoe@mail.com"
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
+    let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "********"
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setProfileView()
+    }
+    
+    func setProfileView() {
         view.backgroundColor = UIColor.mainColor()
+        navigationController?.isNavigationBarHidden = true
+        
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor.secondaryColor()
+        containerView.layer.cornerRadius = 10
+        containerView.customBorder()
+        let height = (view.frame.height) - 300
+        
+        view.addSubview(containerView)
+        containerView.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, padding: .init(top: 0, left: 10, bottom: 10, right: 10), size: .init(width: 0, height: height))
+        
+        setUserInfo(containerView)
+    }
+    
+    func setUserInfo(_ containerView: UIView) {
+        let usernameBottonLineView = UIView()
+        usernameBottonLineView.backgroundColor = .black
+        
+        let emailBottonLineView = UIView()
+        emailBottonLineView.backgroundColor = .black
+        
+        containerView.addSubview(usernameLabel)
+        usernameLabel.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, padding: .init(top: 30, left: 10, bottom: 0, right: 10), size: .init(width: 0, height: 30))
+        containerView.addSubview(usernameBottonLineView)
+        usernameBottonLineView.anchor(top: usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: nil, right: usernameLabel.rightAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 5))
+        
+        containerView.addSubview(emailLabel)
+        emailLabel.anchor(top: usernameBottonLineView.bottomAnchor, left: usernameBottonLineView.leftAnchor, bottom: nil, right: nil, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 250, height: 30))
+        containerView.addSubview(emailBottonLineView)
+        emailBottonLineView.anchor(top: emailLabel.bottomAnchor, left: emailLabel.leftAnchor, bottom: nil, right: emailLabel.rightAnchor, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 5))
     }
 }
